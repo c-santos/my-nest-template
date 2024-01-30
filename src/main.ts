fc7abd4ea25a567539586611e61b3db7727028cb
c-santos <jcms121899@gmail.com>
2024-01-30 11:11:06 +0800

    Added Swagger API

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './infrastructure/modules/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('i2i-ms-debit-credit')
+    .setDescription('Project i2i Microservice Debit-Credit API')
+    .setVersion('1.0')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('/api', app, swaggerDocument, {
+    useGlobalPrefix: true,
+  });
+
   await app.listen(3000);
 }
+
 bootstrap();
