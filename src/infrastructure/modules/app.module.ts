9f9f4322ce47f74a7331d8b9bb7cf6d6ea19698a
c-santos <jcms121899@gmail.com>
2024-04-03 23:15:12 +0800

    Import typeorm module to app module

--- src/infrastructure/modules/app.module.ts
+++ src/infrastructure/modules/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from '../http/controllers/app.controller';
 import { AppService } from '../../application/services/app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dbConfig } from '../config/typeorm.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        TypeOrmModule.forRootAsync({
+            useFactory: () => dbConfig,
+        }),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
