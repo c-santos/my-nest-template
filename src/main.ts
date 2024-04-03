4c5221d2b688fb66bb5a00f20bdbbcb9aaca1268
c-santos <jcms121899@gmail.com>
2024-04-03 23:16:47 +0800

    Update swagger

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,20 @@ import { AppModule } from './infrastructure/modules/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  const swaggerConfig = new DocumentBuilder()
-    .setTitle('i2i-ms-debit-credit')
-    .setDescription('Project i2i Microservice Debit-Credit API')
-    .setVersion('1.0')
-    .build();
+    const swaggerConfig = new DocumentBuilder()
+        .setTitle('my-nestjs-template')
+        .setDescription('My Nest Template API')
+        .setVersion('1.0')
+        .build();
 
-  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('/api', app, swaggerDocument, {
-    useGlobalPrefix: true,
-  });
+    const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('/api', app, swaggerDocument, {
+        useGlobalPrefix: true,
+    });
 
-  await app.listen(3000);
+    await app.listen(3000);
 }
 
 bootstrap();
