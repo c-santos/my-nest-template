0e663da86cbff5c08c4bc2c84b37a0017cb3f7fd
c-santos <jcms121899@gmail.com>
2024-04-05 15:05:19 +0800

    Added config-based port, global TransformResponseInterceptor, and setting of globalPrefix

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './infrastructure/modules/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { configService } from './infrastructure/config/config.service';
+import { TransformResponseInterceptor } from './infrastructure/http/interceptors/transform-response.interceptor';
 
 async function bootstrap() {
+    const port = configService.getValue('PORT') || 8000;
     const app = await NestFactory.create(AppModule);
+
+    app.setGlobalPrefix('my-nest-template');
+    app.useGlobalInterceptors(new TransformResponseInterceptor());
 
     const swaggerConfig = new DocumentBuilder()
         .setTitle('my-nestjs-template')
@@ -16,7 +22,7 @@ async function bootstrap() {
         useGlobalPrefix: true,
     });
 
-    await app.listen(3000);
+    await app.listen(port);
 }
 
 bootstrap();
