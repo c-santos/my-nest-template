5807e23b4d06e9e47c5990a4889f1073a301a3f6
Carlo Santos <jcms121899@gmail.com>
2024-11-29 00:35:28 +0800

    feat: added transactions to base repo (#9)
    * added transactions to base repo
    
    * updated IBaseRepository to include queryRunner arg
    
    * added sample implementation in UserService

--- src/application/services/user.service.ts
+++ src/application/services/user.service.ts
@@ -2,10 +2,13 @@ import { Inject } from '@nestjs/common';
 import { BaseService } from './base.service';
 import { CreateUserDto } from '@/infrastructure/http/dtos/create-user.dto';
 import { IUserRepository } from '@/domain/interfaces/IUserRepository';
+import { DataSource } from 'typeorm';
 
 export class UserService extends BaseService {
     constructor(
         @Inject(IUserRepository) private userRepository: IUserRepository,
+        // Inject DataSource to instantiate queryRunner
+        @Inject(DataSource) private dataSource: DataSource,
     ) {
         super(UserService.name);
     }
@@ -19,6 +22,23 @@ export class UserService extends BaseService {
     }
 
     async createUser(data: CreateUserDto) {
-        return await this.userRepository.create(data);
+        const queryRunner = this.dataSource.createQueryRunner();
+
+        await queryRunner.startTransaction();
+        try {
+
+            // some error-causing logic
+            if (data.email.startsWith('@')) {
+                throw new Error("email can't start with @");
+            }
+
+            await this.userRepository.create(data, queryRunner);
+            await queryRunner.commitTransaction();
+        } catch (error) {
+            console.error(error);
+            await queryRunner.rollbackTransaction();
+        } finally {
+            await queryRunner.release();
+        }
     }
 }
